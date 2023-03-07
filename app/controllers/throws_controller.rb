53d028ee6fb0d725f910f81a0255ab37b85de97d
Paulina Kamińska <paulina.kaminska@energydatalab.com>
2023-03-07 19:05:44 +0100

    Adding html response + refactoring of css

--- app/controllers/throws_controller.rb
+++ app/controllers/throws_controller.rb
@@ -2,13 +2,18 @@
 
 # ThrowsController handles all the actions for the RockPaperScissors game
 class ThrowsController < ApplicationController
-  def new; end
+  def index; end
 
   def create
     @value = params[:value]
 
-    throw = GenerateThrow.new.call
-    result = ::CompareInputs.new(@value, throw).call
-    render plain: "moje: #{@value}, wylosowane: #{throw}, wynik: #{result}"
+    @throw = GenerateThrow.new.call
+    @result = ::CompareInputs.new(@value, @throw).call
+    # render plain: "moje: #{@value}, wylosowane: #{throw}, wynik: #{result}"
+
+    respond_to do |format|
+      format.html { render 'bet_result' }
+      format.js
+    end
   end
 end
