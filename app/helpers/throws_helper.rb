815842296a75b12258ef463f9cdd36ab80d87fe2
Paulina Kamińska <paulina.kaminska@energydatalab.com>
2023-03-07 21:27:03 +0100

    Refactoring: helper & translations

--- app/helpers/throws_helper.rb
+++ app/helpers/throws_helper.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
 module ThrowsHelper
+  def throw_link(element)
+    link_to throws_path(value: element),
+            method: :post,
+            class: "throw-btn throw-btn-js #{element}-btn",
+            data: { value: element } do
+      content_tag(:span, element.capitalize)
+    end
+  end
 end
