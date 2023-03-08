e003495839b81383ef0b42adf4805e83799941da
Paulina Kamińska <paulina.kaminska@energydatalab.com>
2023-03-08 21:04:19 +0100

    Testing - CompareInputs

--- app/commands/compare_inputs.rb
+++ app/commands/compare_inputs.rb
@@ -2,17 +2,24 @@
 
 class CompareInputs
   def initialize(user_input, throw)
+    @valid_values = GenerateThrow::CLASSIC_VALUES.include?(user_input) && GenerateThrow::CLASSIC_VALUES.include?(throw)
     @user_input_index = GenerateThrow::CLASSIC_VALUES.index(user_input)
     @throw_index = GenerateThrow::CLASSIC_VALUES.index(throw)
   end
 
+  # Public method
+  # Determines the result of comparison
+  # Returns:
+  # - nil if given values are invalid
+  # - String: 'draw', 'win' or 'loose' when values can be compared
   def call
+    return nil unless valid_values
     return 'draw' if user_input_index == throw_index
 
     classic_values_compare
   end
 
-  attr_reader :user_input_index, :throw_index
+  attr_reader :user_input_index, :throw_index, :valid_values
 
   private
 
--- spec/commands/compare_inputs_spec.rb
+++ spec/commands/compare_inputs_spec.rb
@@ -1,9 +1,38 @@
 # frozen_string_literal: true
 
-RSpec.describe 'CompareInputs' do
-  subject { CompareInputs.new(user_input, sys_throw) }
+RSpec.describe CompareInputs do
+  subject { described_class.new(user_input, sys_throw) }
+  exclamations = Array.new(15, '!').join('')
+  skip_message = "#{exclamations} Those are doubled tests as an example #{exclamations}"
 
-  context "when inputs are equal" do
+  shared_examples "a proper comparison of inputs" do |user_input, sys_throw, compare_result|
+    let(:user_input) { user_input }
+    let(:sys_throw) { sys_throw }
+
+    it "and returns '#{compare_result}' for user input: '#{user_input}' and system throw: '#{sys_throw}'" do
+      expect(subject.call).to eql compare_result
+    end
+  end
+
+  # draw
+  it_behaves_like 'a proper comparison of inputs', 'rock', 'rock', 'draw'
+  it_behaves_like 'a proper comparison of inputs', 'paper', 'paper', 'draw'
+  it_behaves_like 'a proper comparison of inputs', 'scissors', 'scissors', 'draw'
+  # user wins
+  it_behaves_like 'a proper comparison of inputs', 'rock', 'scissors', 'win'
+  it_behaves_like 'a proper comparison of inputs', 'scissors', 'paper', 'win'
+  it_behaves_like 'a proper comparison of inputs', 'paper', 'rock', 'win'
+  # system wins
+  it_behaves_like 'a proper comparison of inputs', 'scissors', 'rock', 'loose'
+  it_behaves_like 'a proper comparison of inputs', 'paper', 'scissors', 'loose'
+  it_behaves_like 'a proper comparison of inputs', 'rock', 'paper', 'loose'
+  # wrong inputs
+  it_behaves_like 'a proper comparison of inputs', 'rocck', 'paper', nil
+  it_behaves_like 'a proper comparison of inputs', 'rock', 'ppaper', nil
+  it_behaves_like 'a proper comparison of inputs', 'rocck', 'rocck', nil
+
+  # classic tests - doubled tests for draw
+  context "when inputs are equal", skip: skip_message do
     describe "and they are 'rock'" do
       let(:user_input) { 'rock' }
       let(:sys_throw) { 'rock' }
